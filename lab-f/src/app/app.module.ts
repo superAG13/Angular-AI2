e117f1c7ef9123e422c6ca063a74338a9ccb4304
AG <antoni.golabek@gmail.com>
2023-12-19 12:35:12 +0100

    lab f changes

---
+++ lab-f/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { ItemsComponent } from './items/items.component';
+import { UsersComponent } from './users/users.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {JwtModule} from "@auth0/angular-jwt";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartComponent,
+    ItemsComponent,
+    UsersComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => localStorage.getItem('access_token'),
+        allowedDomains: ['labjwt.zecer.wi.zut.edu.pl'],
+      }
+    }),
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
